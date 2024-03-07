840fdfcf126b175ce710111d3f38196135238582
zirui2333 <92755042+zirui2333@users.noreply.github.com>
2024-03-07 15:43:29 -0500

    Create CharData.h

---
+++ src/CharData.h
@@ -0,0 +1,49 @@
+#ifndef CHAR_DATA_
+#define CHAR_DATA_
+
+#include <godot_cpp/classes/resource.hpp>
+#include <godot_cpp/classes/sprite_frames.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+
+namespace godot {
+
+class CharData : public Resource {
+  GDCLASS(CharData, Resource)
+
+private:
+  String name_;
+  int attack_;
+  int health_;
+  int speed_;
+  Ref<SpriteFrames> animation_;
+
+protected:
+  static void _bind_methods();
+
+public:
+  // constructor and destructor
+  CharData();
+  ~CharData();
+
+  // getters and setters
+  String getName() const;
+  void setName(const String name);
+
+  int getAttack() const;
+  void setAttack(const int attack);
+
+  int getHealth() const;
+  void setHealth(const int health);
+
+  int getSpeed() const;
+  void setSpeed(const int speed);
+
+  Ref<SpriteFrames> getAnimation() const;
+  void setAnimation(const Ref<SpriteFrames> anim);
+
+  // print method for debug purposes
+  void printCharacter();
+};
+}
+
+#endif
