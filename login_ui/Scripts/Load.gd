509d9d82049847c9c5faa845ef347d9ddb1a4d39
sharronq <sharronsq@gmail.com>
2024-03-10 14:12:52 -0400

    Completed simple home page and login page

---
+++ login_ui/Scripts/Load.gd
@@ -0,0 +1,13 @@
+extends Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.pressed.connect(self.button_pressed)
+
+func button_pressed():
+	get_tree().change_scene_to_file("res://scenes/login.tscn")
+	print("Login!")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
