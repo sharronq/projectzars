e19aa71ec04c077b25b8b59114b0f650bbea5ec8
sharronq <sharronsq@gmail.com>
2024-03-20 15:24:29 -0400

    Made login screen functional with dummy data

--- login_ui/Scripts/NewScene.gd
+++ login_ui/Scripts/NewScene.gd
@@ -3,7 +3,6 @@ extends Button
 # Dictionary keeps track of scene names and their next scene
 # Key: current scene; Value: next scene
 var scene_dict = {
-	"Login": "res://scenes/LoadSave.tscn",
 	"LoadSave": "res://scenes/GameHome.tscn",
 	"Characters": "res://scenes/CharacterCollection.tscn",
 	"Gamehome": "res://scenes/GameHome.tscn"
@@ -15,13 +14,14 @@ func initialize_button():
 
 # Called when button is pressed, transitions to new scene
 func button_pressed():
-	var path = get_scene_path()
+	var current_tree = get_tree()
+	if current_tree == null:
+		return
+	elif current_tree.get_current_scene().name not in scene_dict:
+		print("Scene not found")
+		return
+	var path = scene_dict[current_tree.get_current_scene().name]
 	get_tree().change_scene_to_file(path)
-
-# Gets the path to the next scene from the dictionary
-func get_scene_path():
-	var scene = get_tree().get_current_scene().name
-	return scene_dict[scene]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -32,3 +32,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
