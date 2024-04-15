f1b4132633cd15dd79580831814fb804c7976b9e
sharronq <sharronsq@gmail.com>
2024-04-15 15:25:04 -0400

    connected battler to navbar

---
+++ game/Scripts/start_pve.gd
@@ -0,0 +1,9 @@
+extends Button
+
+var enemy_level = 0
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://battler/battler.tscn")
+	
+func _on_spin_box_value_changed(value):
+	enemy_level = value
