2491b44410c48f671fe4b0295a21047035803610
sharronq <sharronsq@gmail.com>
2024-04-17 18:35:50 -0400

    added functions to save load screen

--- game/Scripts/NewScene.gd
+++ game/Scripts/NewScene.gd
@@ -7,6 +7,7 @@ var load_save : String
 #index file
 var user_file : Dictionary
 
+@onready var saveload_label = $"../LoadSave/save load text"
 
 # Dictionary keeps track of scene names and their next scene
 # Key: current scene; Value: next scene
@@ -16,13 +17,9 @@ var scene_dict = {
 	"Gamehome": "res://scenes/GameHome.tscn"
 }
 
-
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	initialize_button()
-											  # and calls function when it is pressed
 	var found = SaveLoadManager.check_currentFight_exist()
 	
 #Initialize the index file (database)
@@ -40,19 +37,12 @@ func initialize_button():
 	if get_scene_name() == "Viewcharacters" && self.text == "View characters":
 		self.disabled = true
 
-
-
-
 func get_scene_name():
 	return get_tree().get_current_scene().name
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
-
-
 
 # Called when load button is pressed, transitions to new scene
 func _on_load_1_pressed():
@@ -61,40 +51,45 @@ func _on_load_1_pressed():
 	print("delete, load")
 	load_save = "load"
 
-
 func _on_save_pressed():
 	print("delete, save")
 	load_save = "save"
 	
-
-
-
-
+func _on_slot_1_pressed():
+	game_version = 1
+	if load_save == "load":
+		saveload_label.text = "Loading: Slot 1"
+	elif load_save == "save":
+		saveload_label.text = "Saving to: Slot 1"
+	
 func _on_slot_2_pressed():
 	game_version = 2
-
+	if load_save == "load":
+		saveload_label.text = "Loading: Slot 2"
+	elif load_save == "save":
+		saveload_label.text = "Saving to: Slot 2"
 
 func _on_slot_3_pressed():
 	game_version = 3
-
+	if load_save == "load":
+		saveload_label.text = "Loading: Slot 3"
+	elif load_save == "save":
+		saveload_label.text = "Saving to: Slot 3"
 
 #central control of actual loading or saving file
 func _on_comfirm_pressed():
 	#If no game slot is being selected, prompt the user to select one
 	if(game_version == -1):
 		print("Print a message")
+		saveload_label.text = "Please select a game slot."
 		return
 
-	
 	if(load_save == "save"):
 		SaveLoadManager.Save(game_version)
+	
 	else:
 		#Load the game from Firebase
 		SaveLoadManager.Load(game_version)
-
-
-
-
 
 #Return function by SaveLoadManager.Load()
 #When finish initialize the actual game content from Firebase, direct user to home page
